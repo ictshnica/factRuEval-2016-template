efdb71ad1dab5c82a04b672e19c8d65534da6083
Daria Maglevanaya <maglevanaya.dary@gmail.com>
2016-11-28 20:33:33 +0300

    1 sec

--- org.cxx
+++ org.cxx
@@ -2,8 +2,8 @@
 
 OrgDescr -> AnyWord<kwtype="org_descr">;
 
-QuotedWords -> AnyWord<l-quoted, r-quoted>;
-QuotedWords -> AnyWord<l-quoted, ~r-quoted> AnyWord<l-quoted, ~r-quoted>*AnyWord<~l-quoted, r-quoted>;
+Quoted_Words -> AnyWord<l-quoted, r-quoted>;
+Quoted_Words -> AnyWord<l-quoted, ~r-quoted> Word<~quoted>*AnyWord<~l-quoted, r-quoted>;
 
 Org -> OrgDescr interp(Object.Descr) AnyWord<h-reg1> interp(Object.Name; Object.Type="ORG");
 Org -> OrgDescr interp(Object.Descr) AnyWord<l-quoted, r-quoted> interp(Object.Name; Object.Type="ORG");
