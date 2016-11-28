021fc730317d0d61ef7f0f308cb855f0d8b3b464
Daria Maglevanaya <maglevanaya.dary@gmail.com>
2016-11-28 21:21:11 +0300

    org.cxx

--- org.cxx
+++ org.cxx
@@ -1,11 +1,13 @@
 #encoding "utf-8"
 
 OrgDescr -> AnyWord<kwtype="org_descr">;
+OrgHead -> AnyWord<kwtype="org_head">
 
 Quoted_Words -> AnyWord<l-quoted, r-quoted>;
 Quoted_Words -> AnyWord<l-quoted, ~r-quoted> Word<~quoted>*AnyWord<~l-quoted, r-quoted>;
 
-Org -> OrgDescr interp(Object.Descr) Quoted_Words<h-reg1> interp(Object.Name; Object.Type="ORG");
+Org -> OrgDescr interp(Object.Descr) AnyWord<h-reg1> interp(Object.Name; Object.Type="ORG");
 Org -> OrgDescr interp(Object.Descr) Quoted_Words<l-quoted, r-quoted> interp(Object.Name; Object.Type="ORG");
+Org  -> "правительство" AnyWord<h-regl>;
 
 Root -> Org;
