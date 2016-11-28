8ff35eecb795f8c689d27259382f62714544d1d5
Daria Maglevanaya <maglevanaya.dary@gmail.com>
2016-11-28 19:44:37 +0300

    .gitignore

--- person.cxx
+++ person.cxx
@@ -2,3 +2,7 @@
 
 PersonName -> Word<kwtype="algfio">;
 Person -> PersonName interp (Person.Name);
+ProperName -> Word<h-reg1, gram='имя'>
+              Word<h-reg1, gram='отч'>
+              Word<h-reg1, gram='фам'>;
+Person -> ProperName interp (ProperName);
