a64dda4d01c73219773926bd6b825b6686ee9510
Daria Maglevanaya <maglevanaya.dary@gmail.com>
2016-12-16 23:51:47 +0300

    изменения 1 минута

--- org.cxx
+++ org.cxx
@@ -1,13 +1,13 @@
 #encoding "utf-8"
 
 OrgDescr -> AnyWord<kwtype="org_descr">;
-OrgHead -> AnyWord<kwtype="org_head">
+OrgHead -> AnyWord<kwtype="org_head">;
 
 Quoted_Words -> AnyWord<l-quoted, r-quoted>;
 Quoted_Words -> AnyWord<l-quoted, ~r-quoted> Word<~quoted>*AnyWord<~l-quoted, r-quoted>;
 
 Org -> OrgDescr interp(Object.Descr) AnyWord<h-reg1> interp(Object.Name; Object.Type="ORG");
 Org -> OrgDescr interp(Object.Descr) Quoted_Words<l-quoted, r-quoted> interp(Object.Name; Object.Type="ORG");
-Org  -> "правительство" AnyWord<h-regl>;
+Org  -> OrgHead AnyWord<h-reg1>;
 
 Root -> Org;
